3ddf0e5fb08a2a2a3e3f885b6ee351c283d2112c
Nenyams <neneyamasaki@dyn899-204.eduroam.ic.ac.uk>
2019-01-26 18:18:59 +0000

    adding files

---
+++ map-with-marker/map-with-marker/MapViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MapViewController.swift
+//  map-with-marker
+//
+//  Created by Nene Yamasaki on 26/01/2019.
+//  Copyright © 2019 William French. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var myMap: GMSMapView!
+    @IBOutlet weak var routeUpdate: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        view = mapView
+        myMap.camera = camera
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = myMap
+        
+        self.view.bringSubviewToFront(routeUpdate)
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
